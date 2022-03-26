496933935b70eda125c094fe8ae379f4fdbb5aba
Antonio Serrano Garcia <100795542+Antonio-SeGa@users.noreply.github.com>
2022-03-25 18:58:03 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Coca colaes la bebida y bla bla bla bla">
+    <meta name="keywords" content="bebida, gaseosa, cola">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - Coca Cola</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/favicon.png">
+    <script src="https://kit.fontawesome.com/1eb793e989.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a href="#">
+                <img src="img/logo.svg" alt="" class="logo">
+            </a>
+            <nav>
+                <a href="#inicio">Inicio</a>
+                <a href="#nosotros">Nosotros</a>
+                <a href="#servicios">Servicios</a>
+                <a href="#galeria">Galeria</a>
+                <a href="#contactenos">Contáctanos</a>
+            </nav>
+            <a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+        </div>
+    </header>
+
+    <main>
+        <section id="inicio">
+            <img src="img/bannerprincipal.jpg" alt="">
+            <div class="bloque-inicio">
+                <h1>Bienvenidos a Coca Cola</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo nisi neque reprehenderit libero eveniet quod repellat, ipsam tempora. Doloribus sed iusto ea earum necessitatibus. Sint quod libero dolorem natus obcaecati.
+                </p>
+                <a href="#nosotros" class="boton boton-rojo">Ver mas</a>
+
+            </div>
+        </section>
+
+        <section id="nosotros" class="seccion">
+            <div class="container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad minus dolores iusto, vitae adipisci saepe molestiae quaerat provident, beatae vero veniam quis reprehenderit sit. Quam possimus consequuntur voluptas similique.
+                </p>
+            </div>
+        </section>
+
+        <section id="servicios" class="seccion">
+            <div class="container">
+                <div class="row">
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio1.jpeg" alt="">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 1</h3>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate doloremque quae inventore id natus temporibus eius debitis quis, voluptas ullam ipsa tenetur modi dolore vitae magnam error, consequatur illum harum?
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio2.jpeg" alt="">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 2</h3>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate doloremque quae inventore id natus temporibus eius debitis quis, voluptas ullam ipsa tenetur modi dolore vitae magnam error, consequatur illum harum?
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio3.jpeg" alt="">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 3</h3>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate doloremque quae inventore id natus temporibus eius debitis quis, voluptas ullam ipsa tenetur modi dolore vitae magnam error, consequatur illum harum?
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="galeria">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/latas.jpeg">
+                            <h4>Imagen 1</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpeg">
+                            <h4>Imagen 2</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpeg">
+                            <h4>Imagen 3</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/latas.jpeg">
+                            <h4>Imagen 4</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio3.jpeg">
+                            <h4>Imagen 5</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/latas.jpeg">
+                            <h4>Imagen 6</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contactenos" class="seccion">
+            <iframe width="520" height="400" frameborder="0" scrolling="no" 
+            marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Mexico%20Progreso%20de%20Obreg%C3%B3n+(Hola)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='http://maps-website.com/es'>maps generator</a> <script type='text/javascript' 
+            src='https://embedmaps.com/google-maps-authorization/script.js?id=cdd5128d537a0c918b50b23055e600dfbc9241c3'></script>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+                        
+                        <form action="index.php" method="post">
+                            <div class="form-block">
+                                <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                            </div>
+
+                            <div class="form-block">
+                                <input type="email" name="email" class="form-control" placeholder="Email">
+                            </div>
+
+                            <div class="form-block">
+                                <textarea name="mensaje" placeholder="Mensaje"></textarea>
+                            </div>
+
+                            <div class="form-block">
+                                <input type="submit" class="boton boton-negro" value="Enviar">
+                            </div>
+                            <?php
+                                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                                    $nombre = $_POST["nombre"];
+                                    $email = $_POST["email"];
+                                    $mensaje = $_POST["mensaje"];
+
+                                    if(isset($nombre)){
+                                        if(isset($email)){
+                                            if(isset($mensaje)){
+                                                $para = "antonioserrano311@gmail.com";
+                                                $asunto ="Esta es una prueba";
+                                                $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                                                $adicionl = "From: antonio123@gmail.com";
+
+                                                mail($para,$asunto,$cuerpo,$adicionl);
+                                            ?>
+                                                <p>Envio exitoso</p>
+                                            <?php
+                                            }
+                                        }
+                                    }
+                                }
+                            ?>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="seccion">
+        <div class="container">
+            <div class="row">
+                <div class="columna columna-25 columna-mobile-100">
+                    <img  src="img/logo-blanco.png" class="logo-footer">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugit itaque ab ducimus.
+                    </p>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Temas relacionados
+                    </h3>
+                    <ul>
+                        <li><a href="#">Tema 1</a></li>
+                        <li><a href="#">Tema 2</a></li>
+                        <li><a href="#">Tema 3</a></li>
+                    </ul>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Datos de contaco
+                    </h3>
+                    <ul>
+                        <li>cocaola2022@gmail.com</li>
+                        <li>+52 5523788912</li>
+                        <li>Emiliano Zapata 40 - Progreso de O. <br> Hidalgo - México</li>
+                    </ul>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                       Redes sociales
+                    </h3>
+                    <ul class="redes">
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-twitter"></i>
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>.
+        <div class="barra-footer">
+            &copy; Derechos Reservados -2022
+        </div>
+    </footer>
+    <script src="js/jqueryjs.js"></script>
+    <script src="js/funciones.js"></script>
+</body>
+</html>
